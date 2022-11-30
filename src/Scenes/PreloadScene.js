6b4cdb03b731f946998bb7aabe7b1bd842964729
Reza Akbar <reza.rezqya@gmail.com>
2022-11-30 22:41:04 +0700

    prepare-preload

--- src/Scenes/PreloadScene.js
+++ src/Scenes/PreloadScene.js
@@ -5,6 +5,8 @@ export default class PreloadScene extends Phaser.Scene {
     super("preload");
   }
   preload() {
+    this.progressGrap = this.add.graphics();
+
     this.load.svg("vite", "vite.svg");
     this.load.svg("js", "javascript.svg");
     //this.load.setBaseURL("https://labs.phaser.io");
@@ -25,8 +27,30 @@ export default class PreloadScene extends Phaser.Scene {
     //sound here
     this.load.audio("bgm", "assets/bgm/happy_adveture.mp3");
     this.load.audio("impact", "assets/sfx/impact.mp3");
+
+    this.load.on("progress", this.updateProgress, this);
+    this.load.on("complete", this.completeLoad, this);
+
+    this.load.on("filecomplete", function (key, type, data) {
+      console.log(key + " " + type + " " + data);
+    });
+    //this.load.update();
+  }
+  updateProgress(value) {
+    console.log(value);
+    this.progressGrap.clear();
+    this.progressGrap.fillStyle(0xffffff, 1);
+    this.progressGrap.fillRect(0, 270, 200 * value, 60);
+  }
+  completeLoad() {
+    console.log("complete load");
+    this.progressGrap.destroy();
   }
   create() {
+    console.log("prepare jump to menu");
     this.scene.start("menu");
+  }
+  update() {
+    console.log("update game");
   }
 }
